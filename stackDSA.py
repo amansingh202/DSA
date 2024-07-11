0b9a52b4d5ac6d56ba57c139c47ece0ccf0f52fe
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-07-11 14:22:22 -0500

    stacks using list

---
+++ stackDSA.py
@@ -0,0 +1,13 @@
+#stacks using lists
+
+s = []
+s.append('https://www.bbc.com/news')
+s.append('https://www.bbc.com/business')
+s.append('https://www.bbc.com/news/articles/cpv3kz9q167o')
+s.append('https://www.bbc.com/news/topics/c1038wnxypvt')
+
+print(s.pop())
+
+print(s)
+
+print(s.pop())
