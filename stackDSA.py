bd53d8e4b5eebb59591f3cc5fa4acde2bd82b37c
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-07-11 14:28:51 -0500

    stack using dequeue

--- stackDSA.py
+++ stackDSA.py
@@ -6,8 +6,28 @@ s.append('https://www.bbc.com/business')
 s.append('https://www.bbc.com/news/articles/cpv3kz9q167o')
 s.append('https://www.bbc.com/news/topics/c1038wnxypvt')
 
+print(s[-1])
+
 print(s.pop())
 
 print(s)
 
 print(s.pop())
+
+#stacks using dequeue
+
+from collections import deque
+
+stack = deque()
+
+print(dir(stack))
+
+stack.append('https://www.bbc.com/news')
+stack.append('https://www.bbc.com/business')
+stack.append('https://www.bbc.com/news/articles/cpv3kz9q167o')
+stack.append('https://www.bbc.com/news/topics/c1038wnxypvt')
+
+print(stack)
+
+print(stack.pop())
+print(stack)
