3c0a8aaf1bb1b50f6ff1c827ef8a0236ec14882c
amansingh202 <kumaraman08071997@gmail.com>
2025-07-15 21:03:44 -0500

    or, xor operator

--- bit_manipulation/binary_to_decimal.py
+++ bit_manipulation/binary_to_decimal.py
@@ -11,5 +11,20 @@ def binary2dec(n):
     return result
 
 
-print(binary2dec('1011'))
+#print(binary2dec('1011'))
+
+#or operator
+def or_op(n1, n2):
+    result = n1 | n2
+    return result
+
+#print(or_op(11,13))
+
+#xor operator
+def xor_op(n1, n2):
+    result = n1 ^ n2
+    return result
+
+print(xor_op(11,13))
+
 
