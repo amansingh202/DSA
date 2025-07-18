60c7c2106cb272cbea0402a57f6b1aac16f64903
amansingh202 <kumaraman08071997@gmail.com>
2025-07-18 02:52:30 -0500

    stacks using queues

---
+++ stacks_and_Queues/stacks_using_queues.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+        
+
+    def push(self, x: int) -> None:
+        self.q2.append(x)
+
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+
+        self.q1, self.q2 = self.q2, self.q1
+        
+
+    def pop(self) -> int:
+        if self.empty():
+            raise IndexError("empty queue")
+        else:
+            return self.q1.popleft()
+        
+
+    def top(self) -> int:
+        if self.empty():
+            raise IndexError("empty queue")
+        else:
+            return self.q1[0]
+
+        
+
+    def empty(self) -> bool:
+        return not self.q1 
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+
+
+obj = MyStack()
+
+for i in range(3, 12):
+    obj.push(i)
+
+print(obj.pop())
+
+print(obj.top())
