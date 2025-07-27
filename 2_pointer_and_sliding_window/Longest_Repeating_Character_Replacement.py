f853e2a650ca3a2cb8bb7933daa70971edc94325
amansingh202 <kumaraman08071997@gmail.com>
2025-07-26 23:58:48 -0500

    better approach

--- 2_pointer_and_sliding_window/Longest_Repeating_Character_Replacement.py
+++ 2_pointer_and_sliding_window/Longest_Repeating_Character_Replacement.py
@@ -21,9 +21,57 @@ class Solution:
 
         return max_len
     
+    def characterReplacement_better(self, s: str, k: int) -> int:
+        n = len(s)
+        max_len = 0
+        l = 0
+        r = 0
+        freq = [0]*26
+        max_f = 0
+
+        while r < n:
+            index = ord(s[r]) - ord('A')
+            freq[index] += 1
+            max_f = max(max_f, freq[index])
+
+            while ( (r - l + 1) - max_f )> k:
+                freq[ord(s[l]) - ord('A')] -= 1
+                l += 1
+
+            max_len = max(max_len, r - l + 1)
+            r += 1
+
+        return max_len
+    
+    def characterReplacement_mostOptimal(self, s: str, k: int) -> int:
+        n = len(s)
+        max_len = 0
+        l = 0
+        r = 0
+        freq = [0]*26
+        max_f = 0
+
+        while r < n:
+            index = ord(s[r]) - ord('A')
+            freq[index] += 1
+            max_f = max(max_f, freq[index])
+
+            if ( (r - l + 1) - max_f )> k:
+                freq[ord(s[l]) - ord('A')] -= 1
+                l += 1
+
+            if (r - l + 1) - max_f <= k:
+                max_len = max(max_len, r - l + 1)
+            r += 1
+
+        return max_len
+    
+
+
+    
 obj = Solution()
 
 s = "AABABBA"
 k = 1
 
-print(obj.characterReplacement(s, k))
+print(obj.characterReplacement_mostOptimal(s, k))
