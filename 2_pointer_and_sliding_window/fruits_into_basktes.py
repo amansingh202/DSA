b1977881bec1fc77069c5693f24947f6de3ac0b9
amansingh202 <kumaraman08071997@gmail.com>
2025-07-26 22:01:01 -0500

    optimal approach

--- 2_pointer_and_sliding_window/fruits_into_basktes.py
+++ 2_pointer_and_sliding_window/fruits_into_basktes.py
@@ -47,6 +47,33 @@ class Solution:
             r += 1
 
         return max_len
+    
+    # most optimal approach
+    def totalFruits_optimal(self, fruits):
+        n = len(fruits)
+        l = 0
+        r = 0
+        max_len = 0
+        mpp = {}
+
+        while r < n:
+            mpp[fruits[r]] = mpp.get(fruits[r], 0) + 1
+
+            if len(mpp) > 2:
+                mpp[fruits[l]] -= 1
+
+                if mpp[fruits[l]] == 0:
+                    del mpp[fruits[l]]
+
+                l += 1
+            
+            if len(mpp) <= 2:
+                max_len = max(max_len, r - l + 1)
+
+            r += 1
+
+        return max_len
+
 
 
 
@@ -55,6 +82,6 @@ obj = Solution()
 
 fruits = [1, 2, 3, 2, 2]
 
-print(obj.totalFruits_better(fruits))
+print(obj.totalFruits_optimal(fruits))
 
         
