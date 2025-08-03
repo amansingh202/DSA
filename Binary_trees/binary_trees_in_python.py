2139f29f5851b4d1b0787ba63b3ff99a4c3de6c6
amansingh202 <kumaraman08071997@gmail.com>
2025-08-02 19:18:00 -0500

    in order tree traveersal

--- Binary_trees/binary_trees_in_python.py
+++ Binary_trees/binary_trees_in_python.py
@@ -11,16 +11,37 @@ class BinaryTree:
     def print_tree(self, traversal_type):
         if traversal_type == 'pre_order':
             return self.preorder_print(tree.root, "")
+        elif traversal_type == "in_order":
+            return self.inorder_traversal(tree.root, "")
 
-    ## preorder traversal DFS
-
+    ## Tree structure
+    # """                             1
+    #                         /                \ 
+    #                        2                   3
+    #                       /  \                / \
+    #                     4      5             6    7     """
+                             
+    ##pre-order tree traversal code
     def preorder_print(self, start, traversal):
         """Root -> left child --> right child"""
+        """ 1->2->4->5->3->6->7"""
 
         if start:
             traversal += str(start.value) + "--"
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
+
+        return traversal
+    
+    ## in-order tree traversal code
+    def inorder_traversal(self, start, traversal):
+        """ left --> root --> right"""
+        """ 4--2--5--1--6--3--7"""
+
+        if start:
+            traversal = self.inorder_traversal(start.left, traversal)
+            traversal += str(start.value) + "--"
+            traversal = self.inorder_traversal(start.right, traversal)
 
         return traversal
 
@@ -35,4 +56,6 @@ tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
 print(tree.print_tree("pre_order"))
+
+print(tree.print_tree("in_order"))
 
