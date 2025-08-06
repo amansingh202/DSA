9373826ddaeb44aec820903b37bf349323408a93
amansingh202 <kumaraman08071997@gmail.com>
2025-08-06 17:22:45 -0500

    Morris_Preorder_Traversal code strivers

---
+++ Binary_trees/Morris_Preorder_Traversal.py
@@ -0,0 +1,50 @@
+## strivers Morris Preorder Traversal
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.data = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorder(self, root):
+        #your code goes here
+
+        result = []
+
+        current = root
+
+        while current:
+            if not current.left:
+                result.append(current.data)
+                current = current.right
+
+            else:
+                prev = current.left
+
+                while(prev.right and prev.right != current):
+                    prev = prev.right
+
+                if not prev.right:
+                    prev.right = current
+                    result.append(current.data)
+                    current = current.left
+                else:
+                    prev.right = None
+                    current = current.right
+
+        return result
+    
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+# Run Morris Inorder Traversal
+obj = Solution()
+print(obj.preorder(root))
