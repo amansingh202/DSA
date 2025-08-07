b2db328fde96e41b42c703c21e76e9ef497ab8e4
amansingh202 <kumaraman08071997@gmail.com>
2025-08-06 20:47:34 -0500

    find minimum

---
+++ Binary_search/find_min_max_in_BST.py
@@ -0,0 +1,35 @@
+## strivers code find_min_max_in_BST
+
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def find_minimum(self, root):
+
+        if not root:
+            return None
+        
+        while root.left:
+            root = root.left
+
+        return root.val
+    
+root = TreeNode(8)
+root.left = TreeNode(3)
+root.right = TreeNode(10)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(4)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(14)
+root.right.right.left = TreeNode(13)
+
+obj = Solution()
+
+print(obj.find_minimum(root))
